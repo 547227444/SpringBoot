8a8994047b4ae3f786a82cebea48268ee414b2ca
leeo_ly@163.com <leeo_ly@163.com>
2018-04-03 14:41:35 +0800

    111

--- src/main/java/com/liyang/controller/FreemarkerController.java
+++ src/main/java/com/liyang/controller/FreemarkerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class FreemarkerController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/a")
 	public String index(ModelMap map) {
 		System.err.println(">>>>>>>>>>>>>>>>>>>>>");
 		map.put("name", "隔壁老王");
