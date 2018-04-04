a641d5b4553813a19e457a3ef2db5d3e8350850d
leeo_ly@163.com <leeo_ly@163.com>
2018-04-04 11:26:40 +0800

    VFD

--- src/main/java/com/liyang/controller/FreemarkerController.java
+++ src/main/java/com/liyang/controller/FreemarkerController.java
@@ -3,6 +3,7 @@ package com.liyang.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FreemarkerController {
